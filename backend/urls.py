4d0e49e177e703a502b30bbd834ca571e5279423
guli2103 <gulbahorcoder@gmail.com>
2023-04-13 09:08:42 +0500

    crimson

--- backend/urls.py
+++ backend/urls.py
@@ -5,4 +5,6 @@ from .views import *
 
 urlpatterns = [
     path('', index ),
+    path('about/', about , name='about'),
+    path('404/', xato404, name='404')
 ]
--- backend/views.py
+++ backend/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def xato404(request):
+    return render(request, '404.html')
