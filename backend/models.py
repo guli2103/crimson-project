f9f96460010c328ff8152d62600157c09f3b780b
guli2103 <gulbahorcoder@gmail.com>
2023-04-28 04:12:18 +0500

    crimso

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    name = models.CharField(max_length=255)
+    date = models.DateField(auto_now_add=False)
+    username = models.CharField(max_length=255)
+    img = models.ImageField(upload_to='img/', default='img/moychechal.jpg', null=True, blank= True)
+
+
+    def __str__(self):
+        return self.username
+    
+
+class TopPost(models.Model):
+    life = models.CharField(max_length=255)
+    
